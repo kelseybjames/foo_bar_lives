250d7443480b9270fd6fbc7d40c0aa9ca18513c8
Kelsey James <kelseybjames@gmail.com>
2016-03-14 08:47:08 -0700

    basic start

--- comb-sort/combSort.js
+++ comb-sort/combSort.js
@@ -1,6 +1,58 @@
-
+var swap = function(array, i, j) {
+  var x = array[i];
+  array[i] = array[j];
+  array[j] = x;
+}
 
 var combSort = function(array) {
-  // Your code here
+  var gap = array.size;
+  var shrink = 1.3;
+
+  while((gap !== 1) || (swapped === true)) {
+    gap = Math.floor(gap / shrink);
+    if (gap < 1) {
+      gap = 1;
+    };
+
+    var swapped = false;
+
+    for(i = 0; i + gap >= array.size; i++) {
+      if (array[i] > array[i + gap]) {
+        swap(array, i, i + gap);
+        swapped = true;
+      };
+    };
+  };
+  return array;
 };
 
+// Pseudocode: 
+
+// function combsort(array input)
+
+//     gap := input.size //initialize gap size
+//     shrink := 1.3 //set the gap shrink factor
+
+//     loop until gap = 1 and swapped = false
+//         //update the gap value for a next comb. Below is an example
+//         gap := int(gap / shrink)
+//         if gap < 1
+//           //minimum gap is 1
+//           gap := 1
+//         end if
+        
+//         i := 0
+//         swapped := false //see bubblesort for an explanation
+        
+//         //a single "comb" over the input list
+//         loop until i + gap >= input.size //see shellsort for similar idea
+//             if input[i] > input[i+gap]
+//                 swap(input[i], input[i+gap])
+//                 swapped := true // Flag a swap has occurred, so the
+//                                 // list is not guaranteed sorted
+//             end if
+//             i := i + 1
+//         end loop
+       
+//     end loop
+// end function
--- groceries/groceries.js
+++ groceries/groceries.js
@@ -1,2 +1,13 @@
 // Your Code Here!
 
+$(document).ready(function() {
+  $('#submit-button').click( function(event) {
+    event.preventDefault();
+    var groceryItem = $('.grocery-addition').val();
+
+    if (groceryItem.length !== 0) {
+      $('.grocery-list').append("<div class='grocery-item'>" + groceryItem + "</div>");
+    };
+  });
+
+});
